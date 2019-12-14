f6d663454b32760d7a25a2bb57ab7da09f7021ba
CS <CS@Chyngyzkans-MacBook-Pro.local>
2019-12-14 16:47:16 -0600

    key modified

--- key_pair.tf
+++ key_pair.tf
@@ -1,4 +1,4 @@
-resource "aws_key_pair" "deployer" { 
-  key_name   = "deployer" 
-  public_key = "${file("~/.ssh/id_rsa.pub")}" 
+resource "aws_key_pair" "${var.key_name}" { 
+  key_name   = "${var.key_name}" 
+  public_key = "${file("${var.key_name_location}")}" 
 } 
--- s3bucket.tf
+++ s3bucket.tf
@@ -1,13 +1,13 @@
-resource "aws_s3_bucket" "buckets3" { 
-  bucket = "my-tf-test-bucket-chyngyzkan" #its name
-  acl    = "private" 
-
-  tags = { 
-    Name        = "My bucket" 
-    Environment = "Dev" 
-  } 
-  versioning { 
-    enabled = true 
-  } 
-
-} 
+#resource "aws_s3_bucket" "buckets3" { 
+#  bucket = "my-tf-test-bucket-chyngyzkan" #its name
+#  acl    = "private" 
+#
+#  tags = { 
+#    Name        = "My bucket" 
+#    Environment = "Dev" 
+#  } 
+#  versioning { 
+#    enabled = true 
+#  } 
+#
+#} 
