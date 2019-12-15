468023a863a18154faa3e56872a17b32cb33b2f3
CS <CS@Chyngyzkans-MacBook-Pro.local>
2019-12-14 20:23:46 -0600

    sec edit

--- sec_group.tf
+++ sec_group.tf
@@ -4,6 +4,10 @@ resource "aws_security_group" "sec_group" {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
